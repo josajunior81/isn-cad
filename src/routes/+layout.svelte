81072bf712d762a383706ca28bd4637a9d1b26fa
Josafá Soares de Souza Júnior <josafa.souza@serpro.gov.br>
2023-03-30 17:53:27 -0300

    Início do cadastro de discípulos

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,18 +6,25 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+  import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Discípulos Beira Mar</strong>
+        <a href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          </svg>      
+        </a>         
 			</svelte:fragment>
+      <strong class="text-xl uppercase">Discípulos Beira Mar</strong>
 			<svelte:fragment slot="trail">
-			</svelte:fragment>
+        <LightSwitch />
+      </svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<!-- Page Route Content -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,23 @@
-<ul>
-  <li><a href="/churchs">Igrejas nas casas</a></li>
-  <li>Discípulos</li>
-</ul>
+<nav class="list-nav">
+	<!-- (optionally you can provde a label here) -->
+	<ul>
+		<li>
+			<a href="/churchs">
+				<span class="badge bg-primary-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+        </svg>
+        </span>
+				<span class="flex-auto">Igrejas nas casas</span>
+			</a>
+		</li>
+		<li>
+			<a href="/disciples">
+				<span class="badge bg-primary-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+        </svg>
+        </span>
+				<span class="flex-auto">Discípulos</span>
+			</a>
+		</li>    
+	</ul>
+</nav>
